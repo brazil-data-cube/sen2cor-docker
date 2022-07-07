0625e35f648c3ada7c504f86e3e96348b993affc
marujore <rennanmarujo@gmail.com>
2022-07-07 14:04:26 -0300

    Improve Dockerfile and Documentation

--- 2.5.5/run-sen2cor.sh
+++ 2.5.5/run-sen2cor.sh
@@ -10,7 +10,7 @@
 set -e
 shopt -s nullglob
 
-if [ ${1} == "--help" ]; then
+if [[ ${1} == "--help" ]]; then
     echo "Usage: \
     docker run --rm \
     -v /path/to/input-dir/:/mnt/input-dir \
@@ -18,7 +18,7 @@ if [ ${1} == "--help" ]; then
     -v /path/to/CCI4SEN2COR:/mnt/sen2cor-aux/CCI4SEN2COR \
     -v /path/to/L2A_GIPP.xml:${SEN2COR_INSTALL_PATH}/cfg/L2A_GIPP.xml \
     -v /path/to/srtm:/mnt/sen2cor-aux/srtm \
-    brazildatacube/sen2cor:2.9.0 <SENTINEL-2_L1C.SAFE>"
+    brazildatacube/sen2cor:2.5.5 <SENTINEL-2_L1C.SAFE>"
     exit 0
 fi
 
@@ -30,14 +30,30 @@ if [[ ${SAFENAME_L1C} != *.SAFE ]] || [[ ${SAFENAME_L1C:4:6} != MSIL1C ]]; then
     exit 1
 fi
 
+# Create workdir
+if [ -z "${WORKDIR}" ]; then
+    WORKDIR=/mnt/work-dir/
+fi
+mkdir -p ${WORKDIR}
+
+# Ensure that workdir/sceneid is clean
+if [ -d "${WORKDIR}/${SAFENAME_L1C}" ]; then
+    rm -r ${WORKDIR}/${SAFENAME_L1C}
+fi
+
+cp -r ${SAFEDIR_L1C} ${WORKDIR}
+
+SAFENAME_L2A=${SAFENAME_L1C//L1C/L2A}
+
 # load Sen2Cor environment variables
 source ${SEN2COR_INSTALL_PATH}/L2A_Bashrc
 
-mkdir -p ${SEN2COR_OUTPUT_DIR}
+L2A_Process --GIP_L2A ${SEN2COR_GIP_L2A} \
+            ${WORKDIR}/${SAFENAME_L1C}
 
-L2A_Process --resolution 10 \
-            --output_dir ${SEN2COR_OUTPUT_DIR} \
-            --GIP_L2A ${SEN2COR_GIP_L2A} \
-            ${SAFEDIR_L1C}
+
+cp -r ${WORKDIR}/${SAFENAME_L2A} $SEN2COR_OUTPUT_DIR
+rm -r $WORKDIR/${SAFENAME_L2A}
+rm -r $WORKDIR/${SAFENAME_L1C}
 
 exit 0
